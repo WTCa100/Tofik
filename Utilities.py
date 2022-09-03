14692be4a50efa0ae70c25f5c915910ef3105710
Jan Bielawa <janbielawa00@gmail.com>
2022-09-03 21:33:51 +0200

    Added 2 error exception catches.

--- Utilities.py
+++ Utilities.py
@@ -9,6 +9,11 @@ class MakeFile:
         self.str_file_name = xlsx_file_name
 
     def actual_convert(self, path_in = ".\\xlsxRawFiles\\", path_out=".\\csvOutFiles\\"):
-        csv_file_name = self.str_file_name[:self.str_file_name.find("xlsx")] + "csv"
-        old_data = pandas.read_excel(path_in + self.str_file_name, index_col=None)
-        old_data.to_csv(path_out + csv_file_name, encoding= 'utf-8')
+        try:
+            csv_file_name = self.str_file_name[:self.str_file_name.find("xlsx")] + "csv"
+            old_data = pandas.read_excel(path_in + self.str_file_name, index_col=None)
+            old_data.to_csv(path_out + csv_file_name, encoding= 'utf-8')
+        except ValueError:
+            print(f"{self.str_file_name} seems to be broken.")
+        except PermissionError:
+            print(f"{self.str_file_name} is being used in another program.")
