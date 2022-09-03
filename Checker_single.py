9889bd6560f2d399aca893c8b50431e181c7452a
Jan Bielawa <janbielawa00@gmail.com>
2022-09-03 18:32:14 +0200

    Fixed the searching algorithm in Checker_single.py

--- Checker_single.py
+++ Checker_single.py
@@ -10,14 +10,18 @@ class Filer:
         self.str_file_name = name_value
 
     def check_file_name(self):
-        return re.search(".+\.xlsx", self.str_file_name)
+        indexLastDot = self.str_file_name.rfind(".")
+        strActExt = self.str_file_name[indexLastDot:]
+        if indexLastDot == -1 or re.search(strActExt, "\.xlsx") is None: # Yes it's overcomplicated
+            return False
+        return True
 
     def look_for_file(self):
-        print("Looking for file...", end=' ')
-        if self.check_file_name() is None:
+        if not self.check_file_name():
             print("The file is not .xlsx type!")
             return False
         else:
+            print("Looking for file...", end=' ')
             str_file_path = os.path.abspath(os.getcwd()) + "\\xlsxRawFiles\\"
             if not os.path.exists(str_file_path + self.str_file_name):
                 print(f"There is no such file in this directory! dir: {str_file_path}")
